d26b61b1007f4b6f02b2a97a932953652f26d76b
Mr. Marchinek <jason.marchinek@nwsc.k12.in.us>
2024-10-15 10:53:56 -0400

    Update script.js
    Added ability to clear click counts.

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ function buttonClicked() {
     numButtonClicks = numButtonClicks + 1;
     document.getElementById("buttonDiv").innerHTML = "Button clicked: <strong>" + numButtonClicks + "</strong> times";
 }
+
+function clearClicks() {
+	numButtonClicks = 0;
+	numCookieClicks = 0;
+	document.getElementById("cookieDiv").innerHTML = "Click the cookie below:";
+	document.getElementById("buttonDiv").innerHTML = "Click the button below:";
+}
